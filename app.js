1409bbac939ba5d6b80a8ed3bc25766ed7eb6026
alejovillegas7 <alejovillegaslopez@gmail.com>
2019-11-21 16:39:24 -0500

    add mongo Atlas url to deploy on heroku

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentsRoutes = require('./routes/comments');
 var campgroundsRoutes = require('./routes/campgrounds');
 var authRoutes = require('./routes/index');
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect("mongodb+srv://alejovillegas7:mongopassword@yelpcamp-cvmcp.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
